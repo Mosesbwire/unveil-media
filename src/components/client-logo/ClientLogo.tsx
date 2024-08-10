3da8b102b9888437abbd91fe744de9614ff238aa
Mosesbwire <mosesdev92@gmail.com>
2024-08-10 14:45:58 +0300

    feat: add client logos trust symbols

---
+++ src/components/client-logo/ClientLogo.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './client-logo.css'
+
+type imgLogoUrl = {
+    url: string
+    altTxt: string
+}
+
+const ClientLogo = ({url, altTxt}:imgLogoUrl) => {
+  return (
+    <div className='client_logo'>
+        <img src={url} alt={altTxt} />
+    </div>
+  )
+}
+
+export default ClientLogo
